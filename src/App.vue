5a014f4499379cf3176b5f00f9e1f4c3c0d70a9b
0xcircuitbreaker <sarris88p@gmail.com>
2023-11-14 15:00:20 -0500

    YNG ui updates

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="bg-custom-blue min-h-screen text-white flex flex-col items-center py-10">
+  <!-- Main Container -->
+  <div class="bg-custom-blue min-h-screen text-white flex flex-col items-center py-10 overflow-hidden">
     <!-- Logo as Home Button -->
     <a href="." class="absolute top-0 left-0 m-4">
       <img src="@/assets/logo.png" alt="Company Logo" class="h-12 md:h-16 lg:h-20">
     </a>
     <!-- Title -->
-    <h1 class="font-massive font-semibold uppercase mt-8 font-unbounded text-shadow-default">Make - Mint - Monetize</h1>
-
+    <h1 class="font-massive text-5xl sm:text-5xl md:text-5xl lg:text-5xl font-semibold uppercase mt-8 font-unbounded text-shadow-default px-4 sm:px-0">Make - Mint - Monetize</h1>
     <!-- Body Text -->
-    <p class="text-lg font-regular mt-4 mb-16 font-poppins text-shadow-default">
+    <p class="text-lg font-regular mt-4 mb-16 font-poppins text-shadow-default px-4 sm:px-0">
       All in our <span class="text-custom-yellow">simple to use</span> software | Sign up NOW for <span class="text-custom-yellow">early access</span> and discounts
     </p>
-
-     <!-- Main Container for Guru, surrounding elements, and Email Input -->
-     <div class="relative flex flex-col items-center w-96 h-80">
+      <!-- Main Container for Guru, surrounding elements, and Email Input -->
+      <div class="relative flex flex-col items-center w-full max-w-md h-80">
       <!-- Container for Sun Circle, L shot, R shot, Guru Image, and Unlock button -->
       <div class="absolute z-10 w-72 h-72">
-
         <!-- Images behind Sun Circle -->
         <div class="absolute top-0 left-0 w-full h-full flex justify-center items-center z-0">
           <img src="@/assets/lshot.png" alt="L shot" class="h-full object-cover absolute left-0 transform -translate-x-[60%]">
@@ -24,84 +22,70 @@
         </div>
         <!-- Sun Circle -->
         <div class="bg-custom-yellow w-full h-full rounded-full absolute z-10 shadow-custom-dark"></div>
-        <!-- Guru Image -->
-        <img src="@/assets/guru.png" alt="Guru" class="z-20 absolute -top-10 left-0 w-full h-auto" />
-
-        <!-- Unlock Button
-        <img src="@/assets/unlockbutton.png" alt="Unlock" class="z-30 w-24 h-24 absolute bottom-6 left-4" /> -->
+          <!-- Guru Image -->
+        <img src="@/assets/E-guru.gif" alt="Guru" 
+          style="width: auto; height: 400px; max-width: none; max-height: none !important;" 
+          class="z-20 absolute -top-10 left-1/2 ml-[-200px] mt-[-5px] md:w-[500px] lg:w-[600px] max-w-none max-h-none" />
       </div>
-
-      <!-- Email Input Field -->
-      <input type="email" placeholder="Enter E-mail Here - Let the Guru guide you" 
-        @keyup.enter="submitToGoogleForms" v-model="email" 
-        class="absolute bg-white p-2 mb-2 rounded bottom-0 z-50 focus:outline-none focus:ring-2 focus:ring-custom-yellow w-11/12 left-1/2 transform -translate-x-1/2 text-black">
-
       <!-- Unlock Button -->
-      <img src="@/assets/unlockbutton.png" alt="Unlock" @click="submitToGoogleForms" 
-        class="cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-150 absolute z-50 h-12 mb-7 bottom-0 left-1 pl-3 transform -translate-x-11/12 -translate-y-1/2">
-    </div>
+      <a href="https://forms.gle/7CX4JGRBCnnPsYZ26" target="_blank" 
+       class="cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-150 ml-[-75px] absolute z-50 mt-1 bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <img src="@/assets/unlockbutton.png" alt="Unlock" class="w-32 h-auto sm:w-18 md:w-24 lg:w-32">
+      </a>
+      </div>
   </div>
 </template>
 
-    <!-- Google Form Embed -->
-    <!-- <div class="mt-10 w-full max-w-xl">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfCA7BTIg3waKOa9TTBFWxROA0F4ISts-4WFvnhZK5AhwfFrA/viewform?embedded=true" width="100%" height="551" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-    </div> -->
-
-
-    <script>
-    export default {
-      name: 'App',
-      data() {
-        return {
-          email: '', // This will bind email input
-        };
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        email: '',
+      };
+    },
+    methods: {
+      isValidEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
       },
-      methods: {
-        isValidEmail(email) {
-          const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-          return re.test(email);
-        },
-        async submitToGoogleForms() {
-          if (this.email === '') {
-            alert("Please enter your email.");
-            return; // Stop the method if the email is empty
-          }
-          if (!this.isValidEmail(this.email)) {
-            alert("Please enter a valid email address.");
-            return; // Stop the method if the email is not valid
-          }
-          const formUrl = 'YOUR_GOOGLE_FORM_ACTION_URL'; // Replace with form action URL
-          const formId = 'YOUR_GOOGLE_FORM_EMAIL_ENTRY_ID'; // Replace with form email entry ID
-
-          const formData = new FormData();
-          formData.append(`entry.${formId}`, this.email);
-
-          try {
-            const response = await fetch(formUrl, {
-              method: 'POST',
-              body: formData,
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-              }
-            });
-
-            if (response.ok) {
-              // Handle the successful submission here
-              console.log('Email submitted to Google Forms');
-              // Optionally, clear the input field
-              this.email = '';
-            } else {
-              // Handle errors here
-              console.error('Submission failed', response.status);
+      async submitToGoogleForms() {
+        if (this.email === '') {
+          alert("Please enter your email.");
+          return; // Stop the method if the email is empty
+        }
+        if (!this.isValidEmail(this.email)) {
+          alert("Please enter a valid email address.");
+          return; // Stop the method if the email is not valid
+        }
+        const formUrl = 'GOOGLE_FORM_ACTION_URL';
+        const formId = 'GOOGLE_FORM_EMAIL_ENTRY_ID';
+        const formData = new FormData();
+        formData.append(`entry.${formId}`, this.email);
+        try {
+          const response = await fetch(formUrl, {
+            method: 'POST',
+            body: formData,
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
             }
-          } catch (error) {
-            console.error('An error occurred while submitting the form', error);
+          });
+          if (response.ok) {
+            // Handle the successful submission here
+            console.log('Email submitted to Google Forms');
+            // Optionally, clear the input field
+            this.email = '';
+          } else {
+            // Handle errors here
+            console.error('Submission failed', response.status);
           }
+        } catch (error) {
+          console.error('An error occurred while submitting the form', error);
         }
       }
     }
-    </script>
+  }
+</script>
 
 <style>
 @font-face {
@@ -118,5 +102,23 @@
 
 .font-massive {
   font-size: 4rem !important;
+}
+
+@media (max-width: 640px) {
+  .font-massive {
+    font-size: 1.75rem !important;
+  }
+}
+
+@media (max-width: 430px) {
+  .font-massive {
+    font-size: 3.25rem !important;
+  }
+}
+
+@media (min-width: 641px) {
+  .font-massive {
+    font-size: 4rem !important;
+  }
 }
 </style>
